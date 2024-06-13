4773246880d67cac3543ee22043f9a60a8ef4133
Agampreet Singh <agam1092005@gmail.com>
2024-06-13 19:23:15 +0530

    New Notebook Sliding Animation and App icon images added

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,11 +1,13 @@
 // ignore_for_file: file_names
 
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lablogic/AdditionalFiles/constants.dart';
-import 'package:lablogic/Pages/Notebooks.dart';
 import 'package:lablogic/Pages/Profile.dart';
+import 'package:lablogic/Pages/Records.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -22,12 +24,109 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     _tabController = TabController(length: 2, vsync: this);
   }
 
+  void showTopModalSheet(BuildContext context) {
+    Navigator.of(context).push(PageRouteBuilder(
+      opaque: false,
+      pageBuilder: (BuildContext context, _, __) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Material(
+            color: Colors.transparent,
+            child: Stack(
+              children: [
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+                  child: Container(
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Dismissible(
+                      key: const Key('dismiss'),
+                      direction: DismissDirection.up,
+                      onDismissed: (direction) {
+                        Navigator.of(context).pop();
+                        HapticFeedback.vibrate();
+                      },
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 0.9,
+                        width: double.infinity,
+                        padding: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.05,
+                            left: 16,
+                            right: 16),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(25),
+                            bottomRight: Radius.circular(25),
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Notebooks",
+                              style: AppBarTextStyle,
+                            ),
+                            const Spacer(),
+                            Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Container(
+                                height: 5,
+                                width: 100,
+                                margin:
+                                    const EdgeInsets.symmetric(vertical: 16),
+                                decoration: const BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(25),
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            // Add other content here
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, -1.0);
+        const end = Offset(0.0, 0.0);
+        const curve = Curves.easeInOut;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: DefaultTabController(
         length: 2,
         child: TabBar(
+          padding: const EdgeInsets.only(bottom: 16),
           controller: _tabController,
           dividerHeight: 0,
           splashBorderRadius: BorderRadius.zero,
@@ -84,12 +183,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
             children: [
               GestureDetector(
                 onTap: () {
-                  Navigator.of(context, rootNavigator: true).push(
-                    CupertinoPageRoute<bool>(
-                      fullscreenDialog: false,
-                      builder: (BuildContext context) => const Notebooks(),
-                    ),
-                  );
+                  showTopModalSheet(context);
                   HapticFeedback.selectionClick();
                 },
                 child: const Text(
@@ -107,20 +201,18 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                   );
                   HapticFeedback.selectionClick();
                 },
-                child: const CircleAvatar(
-                  radius: 25,
-                ),
+                child: const CircleAvatar(),
               ),
             ],
           ),
         ),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: TabBarView(
           controller: _tabController,
           children: const [
-            Text("Records"),
+            Records(),
             Text("Notes"),
           ],
         ),
---
+++ lib/Pages/Records.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:lablogic/AdditionalFiles/constants.dart';
+
+class Records extends StatefulWidget {
+  const Records({super.key});
+
+  @override
+  State<Records> createState() => _RecordsState();
+}
+
+class _RecordsState extends State<Records> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 2,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+              Container(
+                color: Colors.white,
+                height: 80,
+                width: double.maxFinite,
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Center(
+                      child: Icon(
+                        Icons.mic,
+                        color: secondaryColor,
+                      ),
+                    ),
+                    Text(
+                        "|||||       |||||||| |||||||||||||||   |||| |||||||||   |||||    |||"),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Icon(
+                        Icons.cloud_done_outlined,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(),
+            ],
+          );
+        });
+  }
+}
